f44d58303f391d36d92a5f5941c1b4e2d5617c93
Aurélien Porté <aurel.porte@gmail.com>
2023-12-02 14:18:40 +0100

    package: installation fontawesome. JSX et SCSS intégration composant collapse et page à propos

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,12 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons"
+import "../style/collapse.scss"
+
 function Collapse ({title, texte}){
     return(
-        <div>
+        <li className="collapse">
             <div className="barre">
                 <h2>{title}</h2>
-                <button></button>
+                <button>
+                    <FontAwesomeIcon icon={faChevronUp} />
+                </button>
             </div>
             <p>{texte}</p>
-        </div>
+        </li>
     )
 }
 
--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,14 +1,28 @@
 import Collapse from "../components/Collapse";
-import totologo from "../img/kalen-emsley.jpg"
+import "../style/apropos.scss"
 
 function Apropos(){
     return(
-        <main>
-            <img src={totologo} alt="Paysage de montagnes" />
-            <Collapse />
-            <Collapse />
-            <Collapse />
-            <Collapse /> 
+        <main className="apropos">
+            <div className="banner-img"></div>
+            <ul className="valeurs">
+                <Collapse
+                    title="Fiabilité"
+                    texte="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+                />
+                <Collapse
+                    title="Respect"
+                    texte="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+                />
+                <Collapse
+                    title="Service"
+                    texte="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+                />
+                <Collapse
+                    title="Sécurité"
+                    texte="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                />
+            </ul>
         </main>
     )
 }
